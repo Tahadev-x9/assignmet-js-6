45f0245ebac2f67d8512dab35b148a7d1e1644c1
TAHA <dg0882318@gmail.com>
2026-06-02 20:31:12 +0500

    Done

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("hell world")
+
+let student = [
+    {
+        name: "John",
+        age: 20,
+        grade: "A"  
+    },
+{
+    address : "123 Main St",
+    city : "New York",
+    country : "USA" 
+},
+{
+    hobbies : ["reading", "sports", "music"],
+    isStudent : true
+}
+    
+];
+console.log(student)
